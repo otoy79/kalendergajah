474ee6538401056e47c0656055fc8b09c63cb854
Otoy <39596004+otoy79@users.noreply.github.com>
2023-01-18 00:57:21 +0700

    Add files via upload

---
+++ pkb/2023/pkb-js/hal-1.js
@@ -0,0 +1,73 @@
+<div class="ctr">ВАВ І</div>
+<div class="ctr">KETENTUAN UMUM</div> <br>
+<div class="ctr">Pasal 1</div>
+<div class="ctr">PIHAK-PIHAK YANG MEMBUAT</div>
+<div class="ctr">PERJANJIAN KERJA BERSAMA</div>
+<table>
+    <tr>
+        <td class="nm"> 1.</td>
+        <td> Perusahaan PT. Gajah Tunggal Tbk. yang beralamat di Kelurahan Pasirjaya, Kecamatan Jatiuwung, Kota Tangerang, berkantor pusat di Jakarta.</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td> Berdasarkan Akta Notaris Hannywati Gunawan, S.H, nomor: 75, tanggal 14 Agustus 2020. Diwakili oleh:</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>1.&nbsp;&nbsp;FERRY L. HOLLEN</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>Direktur</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>2. &nbsp;&nbsp;KISYUWONO</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>Direktur</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>Selanjutnya disebut sebagai PENGUSAHA.</td>
+    </tr>
+    <tr>
+        <td class="nm">2.</td>
+        <td>Serikat Pekerja Kimia Energi dan Pertambangan pada Unit Kerja PT. Gajah Tunggal Tbk. Tangerang yang terdaftar di Dinas Tenaga Kerja dan Transmigrasi RI melalui Federasi-SPSI.</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>Nomor : SK.043/S:Kep PC.FSP.KEP/SPSI/XII/2018 sesuai Nomor Pencatatan: 560/62-DKK/OP/Kota TNG/VIII/2001</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>Diwakili oleh:</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>1.&nbsp;&nbsp; SAMSUL BAHRI</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>&nbsp;Ketua Umum PUK-FSP-KEP-SPSI PT. Gajah Tunggal Tbk.</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>2.&nbsp;&nbsp; SIGIT PRIHANTORO</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>&nbsp;Sekretaris Umum PUK-FSP-KEP-SPSI PT. Gajah Tunggal Tbk.</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>&nbsp;Selanjutnya disebut sebagai SERIKAT PEKERJA.</td>
+    </tr><br>
+    <tr>
+        <td></td>
+        <td>Telah bersepakat membuat Perjanjian Kerja Bersama untuk Perusahaan PT. Gajah Tunggal Tbk.
+            <n1>
+        </td>
+    </tr>
+</table><br><br><br>
