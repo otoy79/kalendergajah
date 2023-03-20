c70fe4c7cd35bcae9f45282288a047ac025dda65
Otoy <39596004+otoy79@users.noreply.github.com>
2023-03-20 20:42:57 +0700

    Add files via upload

---
+++ imsakiyah/5r.js
@@ -0,0 +1,65 @@
+var tgl = new Date();
+var dd = tgl.getDate();
+
+if (dd==29)
+document.write(' <p class="text">Minggu<br> 2 April 2023<table><tr><td>11 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:31</td></tr><tr><td>Subuh</td><td>: 04:41</td></tr> <tr><td>Dhuhur</td><td> : 12:01</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 18:01</td></tr> <tr><td>Isya`</td><td> : 19:10</td></tr></table> ');
+else if (dd==30)
+document.write(' <p class="text">Senin<br> 3 April 2023<table><tr><td>12 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:31</td></tr><tr><td>Subuh</td><td>: 04:41</td></tr> <tr><td>Dhuhur</td><td> : 12:00</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 18:01</td></tr> <tr><td>Isya`</td><td> : 19:10</td></tr></table> ');
+else if (dd==31)
+document.write(' <p class="text">Selasa<br> 4 April 2023<table><tr><td>13 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:31</td></tr><tr><td>Subuh</td><td>: 04:41</td></tr> <tr><td>Dhuhur</td><td> : 12:00</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 18:01</td></tr> <tr><td>Isya`</td><td> : 19:09</td></tr></table> ');
+else if (dd==1)
+document.write(' <p class="text">Rabu<br> 5 April 2023<table><tr><td>14 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:31</td></tr><tr><td>Subuh</td><td>: 04:41</td></tr> <tr><td>Dhuhur</td><td> : 12:00</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 18:00</td></tr> <tr><td>Isya`</td><td> : 19:09</td></tr></table> ');
+else if (dd==2)
+document.write('  <p class="text">Kamis<br>6 April 2023<table><tr><td>15 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:30</td></tr><tr><td>Subuh</td><td>: 04:40</td></tr> <tr><td>Dhuhur</td><td> : 11:59</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 18:00</td></tr> <tr><td>Isya`</td><td> : 19:08</td></tr></table> ');
+else if (dd==3)
+document.write(' <p class="text">Jum`at<br> 7 April 2023<table><tr><td>16 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:30</td></tr><tr><td>Subuh</td><td>: 04:40</td></tr> <tr><td>Dhuhur</td><td> : 11:59</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:59</td></tr> <tr><td>Isya`</td><td> : 19:08</td></tr></table> ');
+else if (dd==4)
+document.write(' <p class="text">Sabtu<br> 8 April 2023<table><tr><td>17 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:30</td></tr><tr><td>Subuh</td><td>: 04:40</td></tr> <tr><td>Dhuhur</td><td> : 11:59</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:59</td></tr> <tr><td>Isya`</td><td> : 19:08</td></tr></table> ');
+else if (dd==5)
+document.write(' <p class="text">Minggu<br> 9 April 2023<table><tr><td>18 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:30</td></tr><tr><td>Subuh</td><td>: 04:40</td></tr> <tr><td>Dhuhur</td><td> : 11:59</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:58</td></tr> <tr><td>Isya`</td><td> : 19:07</td></tr></table> ');
+else if (dd==6)
+document.write('<p class="text">Senin<br> 10 April 2023<table><tr><td>19 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:30</td></tr><tr><td>Subuh</td><td>: 04:40</td></tr> <tr><td>Dhuhur</td><td> : 11:58</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:58</td></tr> <tr><td>Isya`</td><td> : 19:07</td></tr></table> ');
+else if (dd==7)
+document.write(' <p class="text">Selasa<br> 11 April 2023<table><tr><td>20 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:30</td></tr><tr><td>Subuh</td><td>: 04:40</td></tr> <tr><td>Dhuhur</td><td> : 11:58</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:57</td></tr> <tr><td>Isya`</td><td> : 19:06</td></tr></table> ');
+else if (dd==8)
+document.write(' <p class="text">Rabu<br> 12 April 2023<table><tr><td>21 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:29</td></tr><tr><td>Subuh</td><td>: 04:39</td></tr> <tr><td>Dhuhur</td><td> : 11:58</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:57</td></tr> <tr><td>Isya`</td><td> : 19:06</td></tr></table> ');
+else if (dd==9)
+document.write(' <p class="text">Kamis<br> 13 April 2023<table><tr><td>22 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:29</td></tr><tr><td>Subuh</td><td>: 04:39</td></tr> <tr><td>Dhuhur</td><td> : 11:58</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:57</td></tr> <tr><td>Isya`</td><td> : 19:06</td></tr></table> ');
+else if (dd==10)
+document.write(' <p class="text">Jum`at<br> 14 April 2023<table><tr><td>23 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:29</td></tr><tr><td>Subuh</td><td>: 04:39</td></tr> <tr><td>Dhuhur</td><td> : 11:57</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:56</td></tr> <tr><td>Isya`</td><td> : 19:05</td></tr></table> ');
+else if (dd==11)
+document.write(' <p class="text">Sabtu<br> 15 April 2023<table><tr><td>24 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:29</td></tr><tr><td>Subuh</td><td>: 04:39</td></tr> <tr><td>Dhuhur</td><td> : 11:57</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:56</td></tr> <tr><td>Isya`</td><td> : 19:05</td></tr></table> ');
+else if (dd==12)
+document.write(' <p class="text">Minggu<br> 16 April 2023<table><tr><td>25 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:29</td></tr><tr><td>Subuh</td><td>: 04:39</td></tr> <tr><td>Dhuhur</td><td> : 11:57</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:55</td></tr> <tr><td>Isya`</td><td> : 19:05</td></tr></table> ');
+else if (dd==13)
+document.write(' <p class="text">Senin<br> 17 April 2023<table><tr><td>26 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:29</td></tr><tr><td>Subuh</td><td>: 04:39</td></tr> <tr><td>Dhuhur</td><td> : 11:57</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:55</td></tr> <tr><td>Isya`</td><td> : 19:04</td></tr></table> ');
+else if (dd==14)
+document.write('  <p class="text">Selasa<br> 18 April 2023<table><tr><td>27 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:28</td></tr><tr><td>Subuh</td><td>: 04:38</td></tr> <tr><td>Dhuhur</td><td> : 11:56</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:55</td></tr> <tr><td>Isya`</td><td> : 19:04</td></tr></table> ');
+else if (dd==15)
+document.write(' <p class="text">Rabu<br> 19 April 2023<table><tr><td>28 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:28</td></tr><tr><td>Subuh</td><td>: 04:38</td></tr> <tr><td>Dhuhur</td><td> : 11:56</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:54</td></tr> <tr><td>Isya`</td><td> : 19:04</td></tr></table> ');
+else if (dd==16)
+document.write(' <p class="text">Kamis<br> 20 April 2023<table><tr><td>29 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:28</td></tr><tr><td>Subuh</td><td>: 04:38</td></tr> <tr><td>Dhuhur</td><td> : 11:56</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:54</td></tr> <tr><td>Isya`</td><td> : 19:04</td></tr></table> ');
+else if (dd==17)
+document.write(' <p class="text">Jum`at<br> 21 April 2023<table><tr><td>30 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:28</td></tr><tr><td>Subuh</td><td>: 04:38</td></tr> <tr><td>Dhuhur</td><td> : 11:56</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 17:54</td></tr> <tr><td>Isya`</td><td> : 19:03</td></tr></table> ');
+else if (dd==18)
+document.write(' <p class="text">Sabtu<br> 22 April 2023 <h2> 𝒮ℯ𝓁𝒶𝓂𝒶𝓉 ℋ𝒶𝓇𝒾 ℛ𝒶𝓎𝒶 ℐ𝒹𝓊𝓁 ℱ𝒾𝓉𝓇𝒾 1444ℋ  </h2> ');
+else if (dd==19)
+document.write(' <p class="text">Kamis<br> 23 Maret 2023<table><tr><td>1 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:33</td></tr><tr><td>Subuh</td><td>: 04:43</td></tr> <tr><td>Dhuhur</td><td> : 12:04</td></tr><tr><td>Ashar</td><td> : 15:14</td></tr><tr><td>Maghrib</td><td> : 18:06</td></tr> <tr><td>Isya`</td><td> : 19:15</td></tr></table> ');
+else if (dd==20)
+document.write(' <p class="text">Jum`at<br> 24 Maret 2023<table><tr><td>2 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:32</td></tr><tr><td>Subuh</td><td>: 04:42</td></tr> <tr><td>Dhuhur</td><td> : 12:03</td></tr><tr><td>Ashar</td><td> : 15:14</td></tr><tr><td>Maghrib</td><td> : 18:06</td></tr> <tr><td>Isya`</td><td> : 19:14</td></tr></table> ');
+else if (dd==21)
+document.write(' <p class="text">Sabtu<br> 25 Maret 2023<table><tr><td>3 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:32</td></tr><tr><td>Subuh</td><td>: 04:42</td></tr> <tr><td>Dhuhur</td><td> : 12:03</td></tr><tr><td>Ashar</td><td> : 15:14</td></tr><tr><td>Maghrib</td><td> : 18:05</td></tr> <tr><td>Isya`</td><td> : 19:14</td></tr></table> ');
+else if (dd==22)
+document.write(' <p class="text">Minggu<br> 26 Maret 2023<table><tr><td>4 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:32</td></tr><tr><td>Subuh</td><td>: 04:42</td></tr> <tr><td>Dhuhur</td><td> : 12:03</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 18:05</td></tr> <tr><td>Isya`</td><td> : 19:13</td></tr></table> ');
+else if (dd==23)
+document.write(' <p class="text">Senin<br> 27 Maret 2023<table><tr><td>5 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:32</td></tr><tr><td>Subuh</td><td>: 04:42</td></tr> <tr><td>Dhuhur</td><td> : 12:02</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 18:04</td></tr> <tr><td>Isya`</td><td> : 19:13</td></tr></table>');
+else if (dd==24)
+document.write(' <p class="text">Selasa<br> 28 Maret 2023<table><tr><td>6 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:32</td></tr><tr><td>Subuh</td><td>: 04:42</td></tr> <tr><td>Dhuhur</td><td> : 12:02</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 18:04</td></tr> <tr><td>Isya`</td><td> : 19:12</td></tr></table> ');
+else if (dd==25)
+document.write(' <p class="text">Rabu<br> 29 Maret 2023<table><tr><td>7 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:32</td></tr><tr><td>Subuh</td><td>: 04:42</td></tr> <tr><td>Dhuhur</td><td> : 12:02</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 18:03</td></tr> <tr><td>Isya`</td><td> : 19:12</td></tr></table> ');
+else if (dd==26)
+document.write(' <p class="text">Kamis<br> 30 Maret 2023<table><tr><td>8 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:32</td></tr><tr><td>Subuh</td><td>: 04:42</td></tr> <tr><td>Dhuhur</td><td> : 12:02</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 18:03</td></tr> <tr><td>Isya`</td><td> : 19:11</td></tr></table> ');
+else if (dd==27)
+document.write(' <p class="text">Jum`at<br> 31 Maret 2023<table><tr><td>9 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:31</td></tr><tr><td>Subuh</td><td>: 04:41</td></tr> <tr><td>Dhuhur</td><td> : 12:01</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 18:02</td></tr> <tr><td>Isya`</td><td> : 19:11</td></tr></table> ');
+else if (dd==28)
+document.write(' <p class="text">Sabtu<br> 1 April 2023<table><tr><td>10 Ramadhan 1444H</td></tr></table></p><table><tr><td>Imsak</td><td> : 04:31</td></tr><tr><td>Subuh</td><td>: 04:41</td></tr> <tr><td>Dhuhur</td><td> : 12:01</td></tr><tr><td>Ashar</td><td> : 15:15</td></tr><tr><td>Maghrib</td><td> : 18:02</td></tr> <tr><td>Isya`</td><td> : 19:10</td></tr></table> ');
