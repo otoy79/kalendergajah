11290d00a13c2e4a788b1ef384e10d09f9c7f7f7
Otoy <39596004+otoy79@users.noreply.github.com>
2026-01-21 19:42:26 +0700

    Add files via upload

---
+++ quran/data2/juz.js
@@ -0,0 +1,182 @@
+window.dataJuz = {
+	"1" : {
+		"beginsurat" : 1,
+		"beginayat" : 1,
+		"endsurat" : 2,
+		"endayat" : 141
+	},
+	"2" : {
+		"beginsurat" : 2,
+		"beginayat" : 142,
+		"endsurat" : 2,
+		"endayat" : 252
+	},
+	"3" : {
+		"beginsurat" : 2,
+		"beginayat" : 253,
+		"endsurat" : 3,
+		"endayat" : 91
+	},
+	"4" : {
+		"beginsurat" : 3,
+		"beginayat" : 92,
+		"endsurat" : 4,
+		"endayat" : 23
+	},
+	"5" : {
+		"beginsurat" : 4,
+		"beginayat" : 24,
+		"endsurat" : 4,
+		"endayat" : 147
+	},
+	"6" : {
+		"beginsurat" : 4,
+		"beginayat" : 148,
+		"endsurat" : 5,
+		"endayat" : 82
+	},
+	"7" : {
+		"beginsurat" : 5,
+		"beginayat" : 83,
+		"endsurat" : 6,
+		"endayat" : 10
+	},
+	"8" : {
+		"beginsurat" : 6,
+		"beginayat" : 11,
+		"endsurat" : 7,
+		"endayat" : 87
+	},
+	"9" : {
+		"beginsurat" : 7,
+		"beginayat" : 88,
+		"endsurat" : 8,
+		"endayat" : 40
+	},
+	"10" : {
+		"beginsurat" : 8,
+		"beginayat" : 41,
+		"endsurat" : 9,
+		"endayat" : 93
+	},
+	"11" : {
+		"beginsurat" : 9,
+		"beginayat" : 94,
+		"endsurat" : 11,
+		"endayat" : 5
+	},
+	"12" : {
+		"beginsurat" : 11,
+		"beginayat" : 6,
+		"endsurat" : 12,
+		"endayat" : 52
+	},
+	"13" : {
+		"beginsurat" : 12,
+		"beginayat" : 53,
+		"endsurat" : 15,
+		"endayat" : 1
+	},
+	"14" : {
+		"beginsurat" : 15,
+		"beginayat" : 2,
+		"endsurat" : 16,
+		"endayat" : 128
+	},
+	"15" : {
+		"beginsurat" : 17,
+		"beginayat" : 1,
+		"endsurat" : 18,
+		"endayat" : 74
+	},
+	"16" : {
+		"beginsurat" : 18,
+		"beginayat" : 75,
+		"endsurat" : 20,
+		"endayat" : 135
+	},
+	"17" : {
+		"beginsurat" : 21,
+		"beginayat" : 1,
+		"endsurat" : 22,
+		"endayat" : 78
+	},
+	"18" : {
+		"beginsurat" : 23,
+		"beginayat" : 1,
+		"endsurat" : 25,
+		"endayat" : 20
+	},
+	"19" : {
+		"beginsurat" : 25,
+		"beginayat" : 21,
+		"endsurat" : 27,
+		"endayat" : 59
+	},
+	"20" : {
+		"beginsurat" : 27,
+		"beginayat" : 60,
+		"endsurat" : 29,
+		"endayat" : 44
+	},
+	"21" : {
+		"beginsurat" : 29,
+		"beginayat" : 45,
+		"endsurat" : 33,
+		"endayat" : 30
+	},
+	"22" : {
+		"beginsurat" : 33,
+		"beginayat" : 31,
+		"endsurat" : 36,
+		"endayat" : 21
+	},
+	"23" : {
+		"beginsurat" : 36,
+		"beginayat" : 22,
+		"endsurat" : 39,
+		"endayat" : 31
+	},
+	"24" : {
+		"beginsurat" : 39,
+		"beginayat" : 32,
+		"endsurat" : 41,
+		"endayat" : 46
+	},
+	"25" : {
+		"beginsurat" : 41,
+		"beginayat" : 47,
+		"endsurat" : 45,
+		"endayat" : 37
+	},
+	"26" : {
+		"beginsurat" : 46,
+		"beginayat" : 1,
+		"endsurat" : 51,
+		"endayat" : 30
+	},
+	"27" : {
+		"beginsurat" : 51,
+		"beginayat" : 31,
+		"endsurat" : 57,
+		"endayat" : 29
+	},
+	"28" : {
+		"beginsurat" : 58,
+		"beginayat" : 1,
+		"endsurat" : 66,
+		"endayat" : 12
+	},
+	"29" : {
+		"beginsurat" : 67,
+		"beginayat" : 1,
+		"endsurat" : 77,
+		"endayat" : 50
+	},
+	"30" : {
+		"beginsurat" : 78,
+		"beginayat" : 1,
+		"endsurat" : 114,
+		"endayat" : 6
+	}
+};
