6e8c55e5376ccdde63196ee79d85ce736ede72af
Otoy <39596004+otoy79@users.noreply.github.com>
2026-02-14 11:48:36 +0700

    Add files via upload

---
+++ musik/musik/denny/denny.js
@@ -0,0 +1,158 @@
+const dennySongs = [
+{
+  title: "  1. Kartonyono_Medot_Janji",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Kartonyono_Medot_Janji.mp3" },
+
+{
+  title: "2. Kok_Iso_Yoo ",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Kok_Iso_Yoo.mp3" },
+
+ {
+  title: "3. Los_Dol..",
+  artist: "Denny Caknan",
+  url:
+  "mp3/LOS_DOL.mp3" },
+
+  {
+  title: "4. Widodari",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Widodari.mp3" },
+
+{
+  title: "5. Satru",
+  artist: "Denny C ft Happy A",
+  url:
+  "mp3/Denny_Happy_Asmara_Satru.mp3" },
+
+{
+  title: "6. Satru-2",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Satru-2.mp3" },
+ {
+  title: "7 Jajalen_Aku",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Jajalen_Aku.mp3" },
+ {
+  title: "8. Lemah_Teles",
+  artist: "Yeni I Ft Denny C",
+  url:
+  "mp3/Yeni_Inka_Ft_Denny_Lemah_Teles.mp3" },
+ {
+  title: "9. Mendem_Wedokan",
+  artist:"Denny Ft Happy A",
+  url:
+  "mp3/Deny_Ft_Happy_Mendem_Wedokan.mp3" },
+ {
+  title: " 10. Stasiun Balapan ",
+  artist: "Denny Caknan",
+  url:
+  "mp3/STASIUN_BALAPAN.mp3" },
+{
+  title: "11. Lamunan",
+  artist: "Denny Caknan",
+  url:
+  "mp3/LAMUNAN.mp3" },
+{
+  title: "12 Teteg Ati",
+  artist: "Denny Caknan",
+  url:
+  "mp3/TETEG_ATI.mp3" },
+ {
+  title: "13. NEMEN",
+  artist: "Deny C ft. Gilga S",
+  url:
+  "mp3/Deny_ft._Gilga_Sahid_NEMEN.mp3" },
+ {
+  title: "14.  KLEBUS",
+  artist: "Denny Caknan",
+  url:
+  "mp3/KLEBUS.mp3" },
+ {
+  title: "15. Sewu Kutho ",
+  artist: "Denny Caknan",
+  url:
+  "mp3/SEWU_KUTHO.mp3" },
+ {
+  title: "16. SANES.",
+  artist: "Denny Caknan",
+  url:
+  "mp3/SANES.mp3" },
+ {
+  title: " 17. WIRANG ",
+  artist: "Denny Caknan",
+  url:
+  "mp3/WIRANG.mp3" },
+ {
+  title: "18. GAMPIL",
+  artist: "Denny Caknan",
+  url:
+  "mp3/GAMPIL.mp3" },
+ {
+  title: " 19. Sigar ",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Sigar.mp3" },
+  {
+  title: "20. Titipane_Gusti",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Titipane_Gusti.mp3" },
+ {
+  title: "21. Sampe_Tuwek",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Sampe_Tuwek.mp3" },
+ {
+  title: "22. Sugeng_Dalu",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Sugeng_Dalu.mp3" },
+ {
+  title: "23. Aisyah_Istri_Rasullah",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Aisyah_Istri_Rasullah.mp3" },
+{
+  title: "24. Lekaslah_Membaik",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Lekaslah_Membaik.mp3" },
+ {
+  title: "25. Tanpo_Tresnamu",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Tanpo_Tresnamu.mp3" },
+ {
+  title: "26. Kangen_Mulih *",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Kangen_Mulih.mp3" },
+   {
+  title: "27. Cundamani.mp3",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Cundamani.mp3" },
+   {
+  title: "28.Jagat_Anyar_Kang_Dumadi",
+  artist: "Soimah_ft_Denny",
+  url:
+  "mp3/Soimah_ft_Denny_Jagat_Anyar_Kang_Dumadi.mp3" },
+  {
+  title: "29. Dalan_Liyane",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Dalan_Liyane.mp3" },
+  {
+  title: "30. Ojo di Banding Bandingke.",
+  artist: "Denny Caknan",
+  url:
+  "mp3/Ojo_di_Bandingke.mp3" },
+  
+];
