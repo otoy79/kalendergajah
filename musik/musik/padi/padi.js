629bd52b81c19db45ad310c770611abb50c20f42
Otoy <39596004+otoy79@users.noreply.github.com>
2026-02-14 10:21:24 +0700

    Add files via upload

---
+++ musik/musik/padi/padi.js
@@ -0,0 +1,147 @@
+const padiSongs = [
+{
+  title: "  ~ Sobat ~ ",
+  artist: "Padi",
+  url:
+  "mp3/Sobat.mp3" },
+
+{
+  title: "Kasih_Tak_Sampai ",
+  artist: "Padi",
+  url:
+  "mp3/Kasih-Tak-Sampai.mp3" },
+
+ {
+  title: "Begitu Indah",
+  artist: "Padi",
+  url:
+  "mp3/Begitu-Indah.mp3" },
+
+  {
+  title: "Seperti_Kekasihku",
+  artist: "Padi",
+  url:
+  "mp3/Seperti-Kekasihku.mp3" },
+
+{
+  title: "Menanti_Sebuah_Jawaban",
+  artist: "Padi",
+  url:
+  "mp3/Padi_Menanti_Sebuah_Jawaban.mp3" },
+
+{
+  title: "Bayangkanlah",
+  artist: "Padi",
+  url:
+  "mp3/Bayangkanlah.mp3" },
+{
+  title: "Hitam",
+  artist: "Padi",
+  url:
+  "mp3/Hitam.mp3" },
+ {
+  title: "Diatas-Bumi-Kita-berpijak",
+  artist: "Padi",
+  url:
+  "mp3/Diatas-Bumi-Kita-berpijak.mp3" },
+ {
+  title: "Cahaya Mata",
+  artist: "Padi",
+  url:
+  "mp3/Cahaya-Mata.mp3" },
+ {
+  title: "~ Lingkaran ~",
+  artist: "Padi",
+  url:
+  "mp3/Lingkaran.mp3" },
+ {
+  title: "* Angkuh *",
+  artist: "Padi",
+  url:
+  "mp3/Angkuh.mp3" },
+{
+  title: "Seandainya-Bisa-Memilih",
+  artist: "Padi",
+  url:
+  "mp3/Seandainya-Bisa-Memilih.mp3" },
+{
+  title: "Perjalanan Ini",
+  artist: "Padi",
+  url:
+  "mp3/Perjalanan-Ini.mp3" },
+ {
+  title: "~ Lain Dunia ~",
+  artist: "Padi",
+  url:
+  "mp3/Lain-Dunia.mp3" },
+ {
+  title: "Kemana-Angin-Berhembus",
+  artist: "Padi",
+  url:
+  "mp3/Kemana-Angin-Berhembus.mp3" },
+ {
+  title: "Disini-Tanpamu",
+  artist: "Padi",
+  url:
+  "mp3/Disini-Tanpamu.mp3" },
+ {
+  title: "• Terlanjur •",
+  artist: "Padi",
+  url:
+  "mp3/Terlanjur.mp3" },
+ {
+  title: "Beri Aku Arti",
+  artist: "Padi",
+  url:
+  "mp3/Beri-Aku-Arti.mp3" },
+ {
+  title: " * Sudahlah *",
+  artist: "Padi",
+  url:
+  "mp3/Sudahlah.mp3" },
+ {
+  title: "Demi Cinta 💕",
+  artist: "Padi",
+  url:
+  "mp3/Demi-Cinta.mp3" },
+ {
+  title: "~ Bidadari ~",
+  artist: "Padi",
+  url:
+  "mp3/Bidadari.mp3" },
+  {
+  title: "Sesuatu Yang Tertunda",
+  artist: "Padi",
+  url:
+  "mp3/Sesuatu-Yang-Tertunda.mp3" },
+ {
+  title: "Repihan Hati",
+  artist: "Padi",
+  url:
+  "mp3/Repihan-Hati.mp3" },
+ {
+  title: "* Rapuh *",
+  artist: "Padi",
+  url:
+  "mp3/Rapuh.mp3" },
+ {
+  title: "~ Patah ~",
+  artist: "Padi",
+  url:
+  "mp3/Patah.mp3" },
+{
+  title: "Manjadi Bijak",
+  artist: "Padi",
+  url:
+  "mp3/Menjadi-Bijak.mp3" },
+ {
+  title: "Menanti-Keajaiban",
+  artist: "Padi",
+  url:
+  "mp3/Menanti-Keajaiban.mp3" },
+ {
+  title: "* Ketakjuban *",
+  artist: "Padi",
+  url:
+  "mp3/Ketakjuban.mp3" },
+];
