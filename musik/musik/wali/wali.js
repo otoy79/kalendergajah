a677f5c87a2351df44b2bdbd0da53992b76b2ea1
Otoy <39596004+otoy79@users.noreply.github.com>
2026-02-14 12:11:47 +0700

    Add files via upload

---
+++ musik/musik/wali/wali.js
@@ -0,0 +1,217 @@
+const waliSongs = [
+{
+  title: "  1. Yank ",
+  artist: "Wali",
+  url:
+  "mp3/1_Yank.mp3" },
+
+{
+  title: "2. Puaskah ",
+  artist: "Wali",
+  url:
+  "mp3/2_Puaskah.mp3" },
+
+ {
+  title: "3. Yank...",
+  artist: "Wali",
+  url:
+  "mp3/3_Dik.mp3" },
+
+  {
+  title: "4.Tobat_Maksiat(Tomat)",
+  artist: "Wali",
+  url:
+  "mp3/4_Tobat_Maksiat(Tomat).mp3" },
+
+{
+  title: "5. Cari_Berkah_CABE",
+  artist: "Wali",
+  url:
+  "mp3/5_Cari_Berkah__CABE.mp3" },
+
+{
+  title: "6. Baik_Baik_Sayang",
+  artist: "Wali",
+  url:
+  "mp3/6_Baik_Baik_Sayang.mp3" },
+ {
+  title: "7. Sayang_Lahir_Batin",
+  artist: "Wali",
+  url:
+  "mp3/7_Sayang_Lahir_Batin.mp3" },
+ {
+  title: "8. Matanyo ~",
+  artist: "Wali",
+  url:
+  "mp3/Matanyo.mp3" },
+ {
+  title: "9. Wasiat_Sang_Kekasih",
+  artist: "Wali",
+  url:
+  "mp3/Wasiat_Sang_Kekasih.mp3" },
+ {
+  title: " 10. Tuhan ",
+  artist: "Wali",
+  url:
+  "mp3/Tuhan.mp3" },
+{
+  title: "11. Ya_Allah",
+  artist: "Wali",
+  url:
+  "mp3/Ya_Allah.mp3" },
+{
+  title: "12 Aku_Cinta_Allah",
+  artist: "Wali",
+  url:
+  "mp3/Aku_Cinta_Allah.mp3" },
+ {
+  title: "13. Mari_Shalawat",
+  artist: "Wali",
+  url:
+  "mp3/Mari_Shalawat.mp3" },
+ {
+  title: "14. Yang_Penting_Halal",
+  artist: "Wali",
+  url:
+  "mp3/Yang_Penting_Halal.mp3" },
+ {
+  title: "15. Salam_Rindu ",
+  artist: "Wali",
+  url:
+  "mp3/Salam_Rindu.mp3" },
+ {
+  title: "16. Langit_Bumi",
+  artist: "Wali",
+  url:
+  "mp3/Langit_Bumi.mp3" },
+ {
+  title: " 17. Nenekku_Pahlawanku ",
+  artist: "Wali",
+  url:
+  "mp3/Nenekku_Pahlawanku.mp3" },
+ {
+  title: "18. Aku_Tidak_Malu",
+  artist: "Wali",
+  url:
+  "mp3/Aku_Tidak_Malu.mp3" },
+ {
+  title: " 19. Masih_Adakah ",
+  artist: "Wali",
+  url:
+  "mp3/Masih_Adakah.mp3" },
+  {
+  title: "20. Jangan_Tuduh_Aku ",
+  artist: "Wali",
+  url:
+  "mp3/Jangan_Tuduh_Aku.mp3" },
+ {
+  title: "21. Doaku_Untukmu_Sayang",
+  artist: "Wali",
+  url:
+  "mp3/Doaku_Untukmu_Sayang.mp3" },
+ {
+  title: "22. Tetap_Bertahan",
+  artist: "Wali",
+  url:
+  "mp3/Tetap_Bertahan.mp3" },
+ {
+  title: "23. Cari_Jodoh",
+  artist: "Wali",
+  url:
+  "mp3/Cari_Jodoh.mp3" },
+{
+  title: "24. Harga_Diriku",
+  artist: "Wali",
+  url:
+  "mp3/Harga_Diriku.mp3" },
+ {
+  title: "25. Ku_Bangga",
+  artist: "Wali",
+  url:
+  "mp3/Ku_Bangga.mp3" },
+ {
+  title: "26. Adinda",
+  artist: "Wali",
+  url:
+  "mp3/Adinda.mp3" },
+   {
+  title: "27. Emang_Dasar",
+  artist: "Wali",
+  url:
+  "mp3/Emang_Dasar.mp3" },
+   {
+  title: "28. Suka_Atau_Tidak",
+  artist: "Wali",
+  url:
+  "mp3/Suka_Atau_Tidak.mp3" },
+  {
+  title: "29. Aku_Bukan_Bang_Toyib",
+  artist: "Wali",
+  url:
+  "mp3/Aku_Bukan_Bang_Toyib.mp3" },
+  {
+  title: "30. Doain_Ya_Penonton. ",
+  artist: "Wali",
+  url:
+  "mp3/Doain_Ya_Penonton.mp3" },
+  {
+  title: "31. Gosok_Batu",
+  artist: "Wali",
+  url:
+  "mp3/Gosok_Batu.mp3" },
+  {
+  title: "32. Jamin_Rasaku",
+  artist: "Wali",
+  url:
+  "mp3/Jamin_Rasaku.mp3" },
+  {
+  title: "33. Cinta_Itu_Amanah(CIA)",
+  artist: "Wali",
+  url:
+  "mp3/Cinta_Itu_Amanah(CIA).mp3" },
+  {
+  title: "34. Takdirkan",
+  artist: "Wali",
+  url:
+  "mp3/Takdirkan.mp3" },
+  {
+  title: " 35. Si_Udin_Bertanya",
+  artist: "Wali",
+  url:
+  "mp3/Si_Udin_Bertanya.mp3" },
+  {
+  title: "36. Indonesia_Juara",
+  artist: "Wali",
+  url:
+  "mp3/Indonesia_Juara.mp3" },
+  {
+  title: "37. Ada_Gajah_Dibalik_Batu",
+  artist: "Wali",
+  url:
+  "mp3/Ada_Gajah_Dibalik_Batu.mp3" },
+  {
+  title: "38. Mengenangmu",
+  artist: "Wali",
+  url:
+  "mp3/Mengenangmu.mp3" },
+  {
+  title: "39. Ditinggal_Kawin",
+  artist: "Wali",
+  url:
+  "mp3/Ditinggal_Kawin.mp3" },
+  {
+  title: "40. Ngantri_Ke_Sorga",
+  artist: "Wali",
+  url:
+  "mp3/Ngantri_Ke_Sorga.mp3" },
+  {
+  title: "41. Obat_Jatuh_Cinta",
+  artist: "Wali",
+  url:
+  "mp3/Obat_Jatuh_Cinta.mp3" },
+  {
+  title: "42. Antara_Aku_Kau_D..",
+  artist: "Wali",
+  url:
+  "mp3/Antara_Aku_Kau_Dan_Batu_Akik.mp3" },
+];
