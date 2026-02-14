e4bff4ae9b1e7affb9e46da021725d97dd5ba4fc
Otoy <39596004+otoy79@users.noreply.github.com>
2026-02-14 12:08:17 +0700

    Add files via upload

---
+++ musik/musik/ungu/ungu.js
@@ -0,0 +1,108 @@
+const unguSongs = [
+{
+  title: "  1. Andai_Ku_Tahu ",
+  artist: "Ungu",
+  url:
+  "mp3/Andai_Ku_Tahu.mp3" },
+
+{
+  title: "2. Asmara_Terindah ",
+  artist: "Ungu",
+  url:
+  "mp3/Asmara_Terindah.mp3" },
+
+ {
+  title: "3. CahayaMu",
+  artist: "Umgu",
+  url:
+  "mp3/CahayaMu.mp3" },
+
+  {
+  title: "4. Dengan_NafasMu",
+  artist: "Ungu",
+  url:
+  "mp3/Dengan_NafasMu.mp3" },
+
+{
+  title: "5. Dia_Maha_Sempurna",
+  artist: "Ungu",
+  url:
+  "mp3/Dia_Maha_Sempurna.mp3" },
+
+{
+  title: "6. Doa",
+  artist: "Ungu",
+  url:
+  "mp3/Doa.mp3" },
+ {
+  title: "7. Doa_Untuk_Ibu.mp3",
+  artist: "Ungu",
+  url:
+  "mp3/Doa_Untuk_Ibu.mp3" },
+ {
+  title: "8. Doa_Yang_Terlupakan",
+  artist: "Ungu",
+  url:
+  "mp3/Doa_Yang_Terlupakan.mp3" },
+ {
+  title: "9. Hanya_Kau",
+  artist: "Ungu",
+  url:
+  "mp3/Hanya_Kau.mp3" },
+ {
+  title: " 10. Hidup_Hanya_Sementar ",
+  artist: "Ungu",
+  url:
+  "mp3/Hidup_Hanya_Sementar.mp3" },
+{
+  title: "11. Ijinkan_Aku",
+  artist: "Ungu",
+  url:
+  "mp3/Ijinkan_Aku.mp3" },
+{
+  title: "12 Maha_Besar",
+  artist: "Ungu",
+  url:
+  "mp3/Maha_Besar.mp3" },
+ {
+  title: "13. Para_PencariMu",
+  artist: "Ungu",
+  url:
+  "mp3/Para_PencariMu.mp3" },
+ {
+  title: "14. Sembah_Sujudku",
+  artist: "Ungu",
+  url:
+  "mp3/Sembah_Sujudku.mp3" },
+ {
+  title: "15. Surga_Hati ",
+  artist: "Ungu",
+  url:
+  "mp3/Surga_Hati.mp3" },
+ {
+  title: "16. Selamat_Lebaran",
+  artist: "Ungu",
+  url:
+  "mp3/Selamat_Lebaran.mp3" },
+ {
+  title: " 17.  SurgaMu ",
+  artist: "Ungu",
+  url:
+  "mp3/SurgaMu.mp3" },
+ {
+  title: "18. Shalawat",
+  artist: "Ungu",
+  url:
+  "mp3/Shalawat.mp3" },
+ {
+  title: " 19. Syukur_Alhamdulillah ",
+  artist: "Ungu",
+  url:
+  "mp3/Syukur_Alhamdulillah.mp3" },
+  {
+  title: "20. Tuhanku.",
+  artist: "Ungu",
+  url:
+  "mp3/Tuhanku.mp3" },
+
+];
