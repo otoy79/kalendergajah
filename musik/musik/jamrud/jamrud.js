8f765a2a23d3805d45855aed5459f118d1d4c455
Otoy <39596004+otoy79@users.noreply.github.com>
2026-02-14 11:45:00 +0700

    Add files via upload

---
+++ musik/musik/jamrud/jamrud.js
@@ -0,0 +1,222 @@
+const jamrudSongs = [
+{
+  title: "  1. Terima_Kasih ~ ",
+  artist: "Jamrud",
+  url:
+  "mp3/Terima_Kasih.mp3" },
+
+{
+  title: "2. Telat_3_Bulan ",
+  artist: "Jamrud",
+  url:
+  "mp3/Telat_3_Bulan.mp3" },
+
+ {
+  title: "3. Surti_Tejo",
+  artist: "Jamrud",
+  url:
+  "mp3/Surti_Tejo.mp3" },
+
+  {
+  title: "4. Selamat_Ulang_Tahun",
+  artist: "Jumrud",
+  url:
+  "mp3/Selamat_Ulang_Tahun.mp3" },
+
+{
+  title: "5. Selamat_Tinggal.mp3",
+  artist: "Jamrud",
+  url:
+  "mp3/Selamat_Tinggal.mp3" },
+
+{
+  title: "6. Reuni_Mantan",
+  artist: "Jamrud",
+  url:
+  "mp3/Reuni_Mantan.mp3" },
+ {
+  title: "7 Rasa_Cinta_Padamu",
+  artist: "Jamrud",
+  url:
+  "mp3/Rasa_Cinta_Padamu.mp3" },
+ {
+  title: "8. Putri ~",
+  artist: "Jamrud",
+  url:
+  "mp3/Putri.mp3" },
+ {
+  title: "9.  Playboy_Kabel ~",
+  artist: "Jamrud",
+  url:
+  "mp3/Playboy_Kabel.mp3" },
+ {
+  title: " 10. Pelangi_Dimatamu ",
+  artist: "Jamrud",
+  url:
+  "mp3/Pelangi_Dimatamu.mp3" },
+{
+  title: "11. Nekad",
+  artist: "Jamrud",
+  url:
+  "mp3/Nekad.mp3" },
+{
+  title: "12 Naksir_Abis",
+  artist: "Jamrud",
+  url:
+  "mp3/Naksir_Abis.mp3" },
+ {
+  title: "13. Masa_Bodo ~",
+  artist: "Jamrud",
+  url:
+  "mp3/Masa_Bodo.mp3" },
+ {
+  title: "13. Maaf_accustic",
+  artist: "Jamrud",
+  url:
+  "mp3/Maaf_accustic.mp3" },
+ {
+  title: "14.  Liar ~",
+  artist: "Jumrud",
+  url:
+  "mp3/Liar.mp3" },
+ {
+  title: "15. Kenyang ",
+  artist: "Jamrud",
+  url:
+  "mp3/Kenyang.mp3" },
+ {
+  title: "16. Kabari_Aku Accustic",
+  artist: "Jamrud",
+  url:
+  "mp3/Kabari_Aku2.mp3" },
+ {
+  title: " 17.  Jaka_Dara ",
+  artist: "Jamrud",
+  url:
+  "mp3/Jaka_Dara.mp3" },
+ {
+  title: "18. Jahanam Bajingan",
+  artist: "Jamrud",
+  url:
+  "mp3/Jahanam__Bajingan.mp3" },
+ {
+  title: " 19. Ingin_Kembali ",
+  artist: "Jamrud",
+  url:
+  "mp3/Ingin_Kembali.mp3" },
+  {
+  title: "20. Emosi_",
+  artist: "Jamrud",
+  url:
+  "mp3/Emosi_35.mp3" },
+ {
+  title: "21. Dokter_Suster",
+  artist: "Jamrud",
+  url:
+  "mp3/Dokter_Suster.mp3" },
+ {
+  title: "22.  Cerita_Jalanan *",
+  artist: "Jamrud",
+  url:
+  "mp3/Cerita_Jalanan.mp3" },
+ {
+  title: "23. Bising ~",
+  artist: "Jamrud",
+  url:
+  "mp3/Bising.mp3" },
+{
+  title: "24. Berakit_Rakit",
+  artist: "Jamrud",
+  url:
+  "mp3/Berakit_Rakit.mp3" },
+ {
+  title: "25. Baywatch",
+  artist: "Jamrud",
+  url:
+  "mp3/Baywatch.mp3" },
+ {
+  title: "26.  Bayang_Dirimu *",
+  artist: "Jamrud",
+  url:
+  "mp3/Bayang_Dirimu.mp3" },
+   {
+  title: "27. Kabari_Aku",
+  artist: "Jamrud",
+  url:
+  "mp3/Kabari_Aku.mp3" },
+   {
+  title: "28. Ayam",
+  artist: "Jamrud",
+  url:
+  "mp3/Ayam.mp3" },
+  {
+  title: "29. Asal_British",
+  artist: "Jamrud",
+  url:
+  "mp3/Asal_British.mp3" },
+  {
+  title: "30. Aral",
+  artist: "Jamrud",
+  url:
+  "mp3/Aral.mp3" },
+  {
+  title: "31. Aku_Benci",
+  artist: "Jamrud",
+  url:
+  "mp3/Aku_Benci.mp3" },
+  {
+  title: "32. 123 Gila",
+  artist: "Jamrud",
+  url:
+  "mp3/123GILA.mp3" },
+  {
+  title: "33. Waktuku_Mandi",
+  artist: "Jamrud",
+  url:
+  "mp3/Waktuku_Mandi.mp3" },
+  {
+  title: "34. Vaksinasi",
+  artist: "Jamrud",
+  url:
+  "mp3/Vaksinasi.mp3" },
+  {
+  title: " 35. Maaf ~",
+  artist: "Jamrud",
+  url:
+  "mp3/Jamrud~Maaf.mp3" },
+  {
+  title: "36. Sakit",
+  artist: "Jamrud",
+  url:
+  "mp3/Sakit.mp3" },
+  {
+  title: "37. Kau_Jahanam_Aku_Bajingan",
+  artist: "Jamrud",
+  url:
+  "mp3/Kau_Jahanam_Aku_Bajingan.mp3" },
+  {
+  title: "38. Pelangi_di_Matamu",
+  artist: "Jamrud",
+  url:
+  "mp3/Pelangi_di_Matamu.mp3" },
+  {
+  title: "39. Maaf",
+  artist: "Jamrud",
+  url:
+  "mp3/Maaf.mp3" },
+  {
+  title: "40. Tentang_Kita",
+  artist: "Jamrud",
+  url:
+  "mp3/Tentang_Kita.mp3" },
+  {
+  title: "41. Shut_Up_N_Listen",
+  artist: "Jamrud",
+  url:
+  "mp3/Shut_Up_N_Listen.mp3" },
+  {
+  title: "42. Rasa-Cinta-Padamu",
+  artist: "Jamrud",
+  url:
+  "mp3/Rasa-Cinta-Padamu.mp3" },
+];
