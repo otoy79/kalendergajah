113dc4c0f01622adf0c7a41926ecb9cf4c85b1d4
Otoy <39596004+otoy79@users.noreply.github.com>
2026-02-14 08:49:52 +0700

    Add files via upload

---
+++ musik/musik/dewa/dewa.js
@@ -0,0 +1,158 @@
+const dewaSongs = [
+{
+  title: "  Hidup-Adalah-Perjuangan ",
+  artist: "Dewa 19",
+  url:
+  "mp3/Hidup-Adalah-Perjuangan.mp3" },
+
+{
+  title: " Mukadimah ",
+  artist: "Dewa 19",
+  url:
+  "mp3/Mukadimah.mp3" },
+
+ {
+  title: "Dua-Sejoli",
+  artist: "Dewa 19",
+  url:
+  "mp3/Dua-Sejoli.mp3" },
+
+  {
+  title: "Risalah-Hati",
+  artist: "Dewa 19",
+  url:
+  "mp3/Risalah-Hati.mp3" },
+
+{
+  title: "Separuh-Nafas",
+  artist: "Dewa 19",
+  url:
+  "mp3/Separuh-Nafas.mp3" },
+
+{
+  title: "1000-Bintang",
+  artist: "Dewa 19",
+  url:
+  "mp3/1000-Bintang.mp3" },
+{
+  title: "Sayap-Sayap-Patah",
+  artist: "Dewa 19",
+  url:
+  "mp3/Sayap-Sayap-Patah.mp3" },
+ {
+  title: "Cinta-Adalah-Misteri",
+  artist: "Dewa 19",
+  url:
+  "mp3/Cinta-Adalah-Misteri.mp3" },
+ {
+  title: "Jalan_Kita_Masih_Panjang",
+  artist: "Dewa 19",
+  url:
+  "mp3/Jalan_Kita_Masih_Panjang.mp3" },
+ {
+  title: "Cinta_kan_Membawamu",
+  artist: "Dewa 19",
+  url:
+  "mp3/Cinta_kan_Membawamu.mp3" },
+ {
+  title: "* Hanya_Satu *",
+  artist: "Dewa 19",
+  url:
+  "mp3/Hanya_Satu.mp3" },
+{
+  title: "Cukup_Siti_Nurbaya",
+  artist: "Dewa 19",
+  url:
+  "mp3/Cukup_Siti_Nurbaya.mp3" },
+{
+  title: "Jangan_Pernah_Mencoba",
+  artist: "Dewa 19",
+  url:
+  "mp3/Jangan_Pernah_Mencoba.mp3" },
+ {
+  title: "~ Hitam_Putih ~",
+  artist: "Dewa 19",
+  url:
+  "mp3/Hitam_Putih.mp3" },
+ {
+  title: "Terbaik-terbaik",
+  artist: "Dewa 19",
+  url:
+  "mp3/Terbaik-terbaik.mp3" },
+ {
+  title: "Satu_Hati_kita_Semestinya",
+  artist: "Dewa 19",
+  url:
+  "mp3/Satu_Hati_kita_Semes.mp3" },
+ {
+  title: "• Larut •",
+  artist: "Dewa. 19",
+  url:
+  "mp3/Larut.mp3" },
+ {
+  title: " • Angin •",
+  artist: "Dewa 19",
+  url:
+  "mp3/Angin.mp3" },
+ {
+  title: " * Kasidah-Cinta *",
+  artist: "Dewa 19",
+  url:
+  "mp3/Kasidah-Cinta.MP3" },
+ {
+  title: "Kosong 💕",
+  artist: "Dewa 19",
+  url:
+  "mp3/Kosong.MP3" },
+ {
+  title: "~ Bukan-Rahasia ~",
+  artist: "Dewa 19",
+  url:
+  "mp3/Bukan-Rahasia.MP3" },
+  {
+  title: "Mistikus-Cinta",
+  artist: "Dewa 19",
+  url:
+  "mp3/Mistikus-Cinta.MP3" },
+ {
+  title: "~ Arjuna ~",
+  artist: "Dewa 19",
+  url:
+  "mp3/Arjuna.mp3" },
+ {
+  title: "* Cinta-Gila *",
+  artist: "Dewa 19",
+  url:
+  "mp3/Cinta-Gila.mp3" },
+ {
+  title: "~ Shine-On ~",
+  artist: "Dewa 19",
+  url:
+  "mp3/Shine-On.mp3" },
+{
+  title: "Nama-Cinta",
+  artist: "Dewa 19",
+  url:
+  "mp3/Nama-Cinta.mp3" },
+ {
+  title: "* Satu *",
+  artist: "Dewa 19",
+  url:
+  "mp3/Satu.mp3" },
+ {
+  title: " Hadapi-Dengan-Senyum ",
+  artist: "Dewa 19",
+  url:
+  "mp3/Hadapi-Dengan-Senyum.mp3" },
+  {
+    title: " Hidup-Ini-Indah ",
+  artist: "Dewa 19",
+  url:
+  "mp3/Hidup-Ini-Indah.mp3" },
+  {
+    title: " Matahari-Bintang-Bulan ",
+  artist: "Dewa 19",
+  url:
+  "mp3/Matahari-Bintang-Bulan.mp3" },
+];
+
