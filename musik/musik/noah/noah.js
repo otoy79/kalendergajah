69fe6f9e72a244bf5f5d7729d8d087e1f2b0a550
Otoy <39596004+otoy79@users.noreply.github.com>
2026-02-14 12:03:18 +0700

    Add files via upload

---
+++ musik/musik/noah/noah.js
@@ -0,0 +1,232 @@
+const noahSongs = [
+{
+  title: "  1. Bintang_Di_Surga",
+  artist: "Noah",
+  url:
+  "mp3/1_Bintang_Di_Surga.mp3" },
+
+{
+  title: "2. Mimpi_Yang_Sempurna ",
+  artist: "Noah",
+  url:
+  "mp3/2_Mimpi_Yang_Sempurna.mp3" },
+
+ {
+  title: "3. Hidup_Untukmu_Mati..",
+  artist: "Noah",
+  url:
+  "mp3/3_Hidup_Untukmu_Mati_Tanpamu.mp3" },
+
+  {
+  title: "4. Separuh_Aku",
+  artist: "Noah",
+  url:
+  "mp3/4_Separuh_Aku.mp3" },
+
+{
+  title: "5. Menunggumu",
+  artist: "Noah",
+  url:
+  "mp3/5_Menunggumu.mp3" },
+
+{
+  title: "6. Topeng",
+  artist: "Noah",
+  url:
+  "mp3/6-Topeng.mp3" },
+ {
+  title: "7 Kita_Tertawa",
+  artist: "Noah",
+  url:
+  "mp3/Kita_Tertawa.mp3" },
+ {
+  title: "8. Mungkin_Nanti",
+  artist: "Noah",
+  url:
+  "mp3/Mungkin_Nanti.mp3" },
+ {
+  title: "9. 2_DSD",
+  artist: "Noah",
+  url:
+  "mp3/2_DSD(Dua_Dalam_Satu_Dunia).mp3" },
+ {
+  title: " 10. Kukatakan_Dgn_Indah ",
+  artist: "Noah",
+  url:
+  "mp3/Kukatakan_Dengan_Indah.mp3" },
+{
+  title: "11. Ada_Apa_Denganmu",
+  artist: "Noah",
+  url:
+  "mp3/Ada_Apa_Denganmu.mp3" },
+{
+  title: "12 Membebaniku",
+  artist: "Noah",
+  url:
+  "mp3/Membebaniku.mp3" },
+ {
+  title: "13. Satu_Hati",
+  artist: "Noah",
+  url:
+  "mp3/Satu_Hati.mp3" },
+ {
+  title: "14.  Sahabat ~",
+  artist: "Noah",
+  url:
+  "mp3/Sahabat.mp3" },
+ {
+  title: "15. Cobalah_Mengerti ",
+  artist: "Noah",
+  url:
+  "mp3/Cobalah_Mengerti.mp3" },
+ {
+  title: "16. Sally_Sendiri",
+  artist: "Noah",
+  url:
+  "mp3/Sally_Sendiri.mp3" },
+ {
+  title: " 17. Hari_Yang_Cerah... ",
+  artist: "Noah",
+  url:
+  "mp3/Hari_Yang_Cerah_Untuk_Jiwa_Yang_Sepi.mp3" },
+ {
+  title: "18. Dan_Hilang",
+  artist: "Noah",
+  url:
+  "mp3/Dan_Hilang.mp3" },
+ {
+  title: " 19. Dibalik_Awan ",
+  artist: "Noah",
+  url:
+  "mp3/Dibalik_Awan.mp3" },
+  {
+  title: "20. Tak_Bisakah",
+  artist: "Noah",
+  url:
+  "mp3/Tak_Bisakah.mp3" },
+ {
+  title: "21. Di_Belakangku.",
+  artist: "Noah",
+  url:
+  "mp3/Di_Belakangku.mp3" },
+ {
+  title: "22. Walau_Habis_Terang *",
+  artist: "Noah",
+  url:
+  "mp3/Walau_Habis_Terang.mp3" },
+ {
+  title: "23. Aku_Dan_ Bintang",
+  artist: "Noah",
+  url:
+  "mp3/Aku_Dan_ Bintang.mp3" },
+{
+  title: "24. Dunia_Yang_Terlupa",
+  artist: "Noah",
+  url:
+  "mp3/Dunia_Yang_Terlupa.mp3" },
+ {
+  title: "25. Taman_Langit",
+  artist: "Peterpen",
+  url:
+  "mp3/Taman_Langit.mp3" },
+ {
+  title: "26. Khayalan_Tingkat_Tinggi *",
+  artist: "Noah",
+  url:
+  "mp3/Khayalan_Tingkat_Tinggi.mp3" },
+   {
+  title: "27. Jauh_Mimpiku",
+  artist: "Noah",
+  url:
+  "mp3/Jauh_Mimpiku.mp3" },
+   {
+  title: "28. Yang_Terdalam",
+  artist: "Noah",
+  url:
+  "mp3/Yang_Terdalam.mp3" },
+  {
+  title: "29. Menghapus_Jejakmu",
+  artist: "Noah",
+  url:
+  "mp3/Menghapus_Jejakmu.mp3" },
+  {
+  title: "30. Tak_Lagi_Sama.",
+  artist: "Noah",
+  url:
+  "mp3/Tak_Lagi_Sama.mp3" },
+  {
+  title: "31. Demi_Kita.",
+  artist: "Noah",
+  url:
+  "mp3/Demi_Kita.mp3" },
+  {
+  title: "32. Ini_Cinta",
+  artist: "Noah",
+  url:
+  "mp3/Ini_Cinta.mp3" },
+  {
+  title: "33. Jika_Engkau",
+  artist: "Noah",
+  url:
+  "mp3/Jika_Engkau.mp3" },
+  {
+  title: "34. Raja_Negeriku",
+  artist: "Noah",
+  url:
+  "mp3/Raja_Negeriku.mp3" },
+  {
+  title: " 35. Tinggallah_Kusendiri",
+  artist: "Noah",
+  url:
+  "mp3/Tinggallah_Kusendiri.mp3" },
+  {
+  title: "36. Sendiri_Lagi",
+  artist: "Noah",
+  url:
+  "mp3/Sendiri_Lagi.mp3" },
+  {
+  title: "37. Sajadah_Panjang",
+  artist: "Noah",
+  url:
+  "mp3/Sajadah_Panjang.mp3" },
+  {
+  title: "38. Kupu_Kupu_Malam",
+  artist: "Noah",
+  url:
+  "mp3/Kupu_Kupu_Malam.mp3" },
+  {
+  title: "39. Cinta_Bukan_Dusta",
+  artist: "Noah",
+  url:
+  "mp3/Cinta_Bukan_Dusta.mp3" },
+  {
+  title: "40. Biar_Ku_Sendiri",
+  artist: "Noah",
+  url:
+  "mp3/Biar_Ku_Sendiri.mp3" },
+  {
+  title: "41. Langit_Tak_Mendengar",
+  artist: "Noah",
+  url:
+  "mp3/Langit_Tak_Mendengar.mp3" },
+  {
+  title: "42. Kirana_Cover_Dewa19",
+  artist: "Noah",
+  url:
+  "mp3/Kirana_Cover_Dewa19.mp3" },
+  {
+  title: "43. Hero.",
+  artist: "Noah",
+  url:
+  "mp3/Hero.mp3" },
+  {
+  title: "44. Dilema_Besar",
+  artist: "Noah",
+  url:
+  "mp3/Dilema_Besar.mp3" },
+  {
+  title: "45. Dara.",
+  artist: "Noah",
+  url:
+  "mp3/Dara.mp3" },
+];
