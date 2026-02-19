83d72ac92fd985638ee432042fa98061cbfd2841
Otoy <39596004+otoy79@users.noreply.github.com>
2026-02-20 05:48:33 +0700

    Add files via upload

---
+++ musik/musik/ungu/religi.js
@@ -0,0 +1,180 @@
+const unguSongs = [
+{
+  title: "  1. Andai_Ku_Tahu ",
+  artist: "Ungu",
+  url:
+  "mp3/Andai_Ku_Tahu.mp3" },
+
+  {
+  title: "Tobat_Maksiat(Tomat)",
+  artist: "Wali",
+  url:
+  "../wali/mp3/4_Tobat_Maksiat(Tomat).mp3" },
+
+{
+  title: "Cari_Berkah_CABE",
+  artist: "Wali",
+  url:
+  "../wali/mp3/5_Cari_Berkah__CABE.mp3" },
+
+{
+  title: "2. Asmara_Terindah ",
+  artist: "Ungu",
+  url:
+  "mp3/Asmara_Terindah.mp3" },
+
+ {
+  title: "3. CahayaMu",
+  artist: "Ungu",
+  url:
+  "mp3/CahayaMu.mp3" },
+  {
+  title: "Ya_Allah",
+  artist: "Wali",
+  url:
+  "../wali/mp3/Ya_Allah.mp3" },
+  {
+  title: "4. Dengan_NafasMu",
+  artist: "Ungu",
+  url:
+  "mp3/Dengan_NafasMu.mp3" },
+
+{
+  title: "5. Dia_Maha_Sempurna",
+  artist: "Ungu",
+  url:
+  "mp3/Dia_Maha_Sempurna.mp3" },
+  {
+  title: "Aku_Cinta_Allah",
+  artist: "Wali",
+  url:
+  "../wali/mp3/Aku_Cinta_Allah.mp3" },
+ {
+  title: "Mari_Shalawat",
+  artist: "Wali",
+  url:
+  "../wali/mp3/Mari_Shalawat.mp3" },
+{
+  title: "6. Doa",
+  artist: "Ungu",
+  url:
+  "mp3/Doa.mp3" },
+ {
+  title: "7. Doa_Untuk_Ibu.mp3",
+  artist: "Ungu",
+  url:
+  "mp3/Doa_Untuk_Ibu.mp3" },
+ {
+  title: " Tuhan ",
+  artist: "Wali",
+  url:
+  "../wali/mp3/Tuhan.mp3" },
+{
+  title: "8. Doa_Yang_Terlupakan",
+  artist: "Ungu",
+  url:
+  "mp3/Doa_Yang_Terlupakan.mp3" },
+  {
+  title: "Baik_Baik_Sayang",
+  artist: "Wali",
+  url:
+  "../wali/mp3/6_Baik_Baik_Sayang.mp3" },
+ {
+  title: "Sayang_Lahir_Batin",
+  artist: "Wali",
+  url:
+  "../wali/mp3/7_Sayang_Lahir_Batin.mp3" },
+ {
+ 
+  title: "9. Hanya_Kau",
+  artist: "Ungu",
+  url:
+  "mp3/Hanya_Kau.mp3" },
+  {
+  title: " Doaku_Untukmu_Sayang",
+  artist: "Wali",
+  url:
+  "../wali/mp3/Doaku_Untukmu_Sayang.mp3" },
+ {
+  title: " 10. Hidup_Hanya_Sementar ",
+  artist: "Ungu",
+  url:
+  "mp3/Hidup_Hanya_Sementar.mp3" },
+  {
+  title: "Aku_Bukan_Bang_Toyib",
+  artist: "Wali",
+  url:
+  "../wali/mp3/Aku_Bukan_Bang_Toyib.mp3" },
+  
+{
+  title: "11. Ijinkan_Aku",
+  artist: "Ungu",
+  url:
+  "mp3/Ijinkan_Aku.mp3" },
+{
+  title: "12 Maha_Besar",
+  artist: "Ungu",
+  url:
+  "mp3/Maha_Besar.mp3" },
+  {
+  title: " Cinta_Itu_Amanah(CIA)",
+  artist: "Wali",
+  url:
+  "../wali/mp3/Cinta_Itu_Amanah(CIA).mp3" },
+ {
+  title: "13. Para_PencariMu",
+  artist: "Ungu",
+  url:
+  "mp3/Para_PencariMu.mp3" },
+  {
+  title: "Takdirkan",
+  artist: "Wali",
+  url:
+  "../wali/mp3/Takdirkan.mp3" },
+ {
+  title: "14. Sembah_Sujudku",
+  artist: "Ungu",
+  url:
+  "mp3/Sembah_Sujudku.mp3" },
+ {
+  title: "15. Surga_Hati ",
+  artist: "Ungu",
+  url:
+  "mp3/Surga_Hati.mp3" },
+  {
+  title: " Ngantri_Ke_Sorga",
+  artist: "Wali",
+  url:
+  "../wali/mp3/Ngantri_Ke_Sorga.mp3" },
+ {
+  title: "16. Selamat_Lebaran",
+  artist: "Ungu",
+  url:
+  "mp3/Selamat_Lebaran.mp3" },
+  {
+  title: "  Yank ",
+  artist: "Wali",
+  url:
+  "../wali/mp3/1_Yank.mp3" },
+ {
+  title: " 17.  SurgaMu ",
+  artist: "Ungu",
+  url:
+  "mp3/SurgaMu.mp3" },
+ {
+  title: "18. Shalawat",
+  artist: "Ungu",
+  url:
+  "mp3/Shalawat.mp3" },
+ {
+  title: " 19. Syukur_Alhamdulillah ",
+  artist: "Ungu",
+  url:
+  "mp3/Syukur_Alhamdulillah.mp3" },
+  {
+  title: "20. Tuhanku.",
+  artist: "Ungu",
+  url:
+  "mp3/Tuhanku.mp3" },
+
+];
